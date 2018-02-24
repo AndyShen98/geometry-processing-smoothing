a9abd19f928397df85f56cc2861d72795c6ddf19
owner <yanzheng.shen@mail.utoronto.ca>
2018-02-23 23:22:13 -0500

    function for smoothing

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -1,10 +1,24 @@
 #include "massmatrix.h"
+#include <igl/doublearea.h>
 
 void massmatrix(
   const Eigen::MatrixXd & l,
   const Eigen::MatrixXi & F,
-  Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M)
-{
-  // Add your code here
+  Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M) {
+  	
+    // compute area for all facets
+	Eigen::VectorXd doublearea(F.rows());
+	igl::doublearea(l, doublearea);
+	Eigen::VectorXd area = 0.5*doublearea;
+	// compute diagonal of M as a vector
+	int V_count = F.maxCoeff() + 1;
+	Eigen::VectorXd diagonal = Eigen::VectorXd::Zero(V_count);
+	for (int i = 0; i < F.rows(); i++) {
+		diagonal(F(i, 0)) += area(i);
+		diagonal(F(i, 1)) += area(i);
+		diagonal(F(i, 2)) += area(i);
+	}
+	diagonal = diagonal / 3.0;
+	M.diagonal() = diagonal;
 }
 
--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,12 +1,36 @@
 #include "smooth.h"
+#include "cotmatrix.h"
+#include "massmatrix.h"
+#include <igl/edge_lengths.h>
 
 void smooth(
     const Eigen::MatrixXd & V,
     const Eigen::MatrixXi & F,
     const Eigen::MatrixXd & G,
     double lambda,
-    Eigen::MatrixXd & U)
-{
-  // Replace with your code
-  U = G;
+    Eigen::MatrixXd & U) {
+
+    // compute edge length
+	Eigen::MatrixXd l;
+	igl::edge_lengths(V, F, l);
+	
+	// compute L and M
+	Eigen::SparseMatrix<double> L;
+	Eigen::DiagonalMatrix<double, Eigen::Dynamic> M;
+	cotmatrix(l, F, L);
+	massmatrix(l, F, M);
+	
+	// solve U
+	Eigen::VectorXd diagM = M.diagonal();
+	typedef Eigen::Triplet<double> T;
+	typedef Eigen::SparseMatrix<double> SpMat;
+	std::vector<T> triplets;
+	triplets.reserve(diagM.size()*2);
+	for (int i = 0; i < diagM.size(); i++) {
+		triplets.push_back(T(i, i, diagM(i)));
+	}
+	SpMat spaM(M.rows(), M.cols());
+	spaM.setFromTriplets(triplets.begin(), triplets.end());
+	Eigen::SimplicialCholesky<Eigen::SparseMatrix<double>> chol(spaM - lambda * L);
+	U = chol.solve(M*G);
 }
